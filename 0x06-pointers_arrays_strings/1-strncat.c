0eaa08aab2cbb5bdef5d86eab16d02173d0143b7
Ing-JB <taguaaye@gmail.coom.com>
2022-09-21 12:59:56 -0700

    concatenate n bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_strncat - copies n bytes of src into dest
+*@src:pointer to source
+*@dest:pointer to destination
+*@n:number of bytes of src to concatenate
+*
+* Return: pointer to dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int c;
+	int d;
+
+	c = 0;
+	while (dest[c] != '\0')
+	{
+		c++;
+	}
+
+	for (d = 0; d < n && src[d]; d++)
+	{
+		dest[c + d] = src[d];
+		d++;
+		c++;
+	}
+	dest[c + d] = '\0';
+	return (dest);
+}
