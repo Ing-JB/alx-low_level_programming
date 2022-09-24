f0d1d0488f973cad4bbf38dfb3a5d5056bda0a76
Ing-JB <taguaaye@gmail.coom.com>
2022-09-24 01:05:40 -0700

    diagonally trapped

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,13 +11,20 @@ void print_diagonal(int n)
 	int i;
 	int j;
 
-	for (i = 0; i < n; i++)
+	if (n <= 0)
 	{
-		for (j = 0; j < i; j++)
-		{
-			_putchar(32);
-		}
-		_putchar(92);
 		_putchar(10);
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(32);
+			}
+			_putchar(92);
+			_putchar(10);
+		}
 	}
 }
