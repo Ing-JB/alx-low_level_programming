3c540f251a6ecafc85ea5436d7297be8412a1849
Ing-JB <taguaaye@gmail.com>
2022-09-24 11:39:54 -0700

    Multplication table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,27 +13,20 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 	{
+		_putchar(' ');
 		for (j = 0; j < 10; j++)
 		{
-			if ((i * j) >= 10)
+			_putchar(' ');
+			_putchar(',');
+			if ((i * j) > 9)
 			{
 				_putchar((i * j) / 10 + '0');
 				_putchar((i * j) % 10 + '0');
-				if ((j < 9) && ((i * j) >= 10))
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
 			}
 			else
 			{
+				_putchar(' ');
 				_putchar((i * j) + '0');
-				if ((j < 9) && ((i * j) < 10))
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
 			}
 
 		}
