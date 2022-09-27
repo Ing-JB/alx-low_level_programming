b713e53d4c7a73c53fada1a20ea9c9ff7dc5fba0
Ing-JB <taguaaye@gmail.com>
2022-09-27 07:26:13 -0700

    Fill up memory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_memset - Fills memory with a constant byte
+*@s:area pointed to by s
+*@b:byte to fill area
+*@n:number of area to fill
+*
+* Return: pointer to dest
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i+=)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+}
